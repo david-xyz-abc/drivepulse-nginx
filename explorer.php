c49fd22ade937340f4b6f0d4fa32d90e32429604
David <davidjohnillickal1@gmail.com>
2025-02-26 18:01:56 +0530

    storage update

--- explorer.php
+++ explorer.php
@@ -215,7 +215,7 @@ if ($currentDir === false || strpos($currentDir, $baseDir) !== 0) {
 }
 
 /************************************************
- * Calculate Storage Usage
+ * Calculate Global Storage Usage
  ************************************************/
 function getDirSize($dir) {
     static $cache = [];
@@ -233,8 +233,12 @@ function getDirSize($dir) {
     return $size;
 }
 
-$totalStorage = 10 * 1024 * 1024 * 1024; // 10 GB in bytes
-$usedStorage = getDirSize($baseDir);
+// Get total server storage
+$totalStorage = disk_total_space("/var/www/html/webdav"); // Gets total disk space
+$freeStorage = disk_free_space("/var/www/html/webdav");   // Gets free disk space
+$usedStorage = $totalStorage - $freeStorage;              // Calculate used space
+
+// Convert to GB for display
 $usedStorageGB = round($usedStorage / (1024 * 1024 * 1024), 2);
 $totalStorageGB = round($totalStorage / (1024 * 1024 * 1024), 2);
 $storagePercentage = round(($usedStorage / $totalStorage) * 100, 2);
